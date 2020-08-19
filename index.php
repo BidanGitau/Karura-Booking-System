8d892bf2d9743245b18a392ecadcb81c3717d146
Bidan Gitau <bydangitau@gmail.com>
2020-08-19 18:21:02 +0300

    landing page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+  <title> Hike and Camp to freedom &mdash; Karura Forest</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <div id="landing">
+      pictorial
+    </div>
+    <div id="article">
+      little know
+    </div>
+    <div id="booking">
+      make a booking
+    </div>
+    <div id="footer">
+
+      welcome more
+    </div>
+  </div>
+</body>
+
+</html>
